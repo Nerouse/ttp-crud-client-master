16cf440d80c947c12bdd32d9ed24d4e61bb98f25
Nerouse <43828177+Nerouse@users.noreply.github.com>
2020-07-16 23:23:22 -0400

    update cases for all campuses

--- src/store/utilities/allCampuses.js
+++ src/store/utilities/allCampuses.js
@@ -6,6 +6,8 @@ import axios from "axios";
 // ACTION TYPES;
 const FETCH_ALL_CAMPUSES = "FETCH_ALL_CAMPUSES";
 const ADD_CAMPUS = "ADD_CAMPUS";
+const EDIT_CAMPUS ="EDIT_CAMPUS";
+const DELETE_CAMPUS = "DELETE_CAMPUS";
 
 // ACTION CREATORS;
 const fetchAllCampuses = (campuses) => {
@@ -21,7 +23,18 @@ const addCampus = (campus) => {
     payload: campus,
   };
 };
-
+const editCampus = (campus) =>{
+  return{
+    type: EDIT_CAMPUS,
+    payload: campus,
+  };
+};
+const deleteCampus = (id) =>{
+  return{
+    type: DELETE_CAMPUS,
+    payload: id,
+  };
+};
 // THUNK CREATORS;
 export const fetchAllCampusesThunk = () => (dispatch) => {
   return axios
@@ -39,6 +52,23 @@ export const addCampusThunk = (campus) => (dispatch) => {
     .catch((err) => console.log(err));
 };
 
+export const editCampusThunk = (id,campus) => (dispatch) =>{
+  return axios
+    .put(`/api/campuses/${id}`, campus)
+    .then((res) => res.data)
+    .then((updatedCampus) => {dispatch(editCampus(updatedCampus)); })
+    .catch((err) => console.log(err));
+
+}; 
+export const deleteCampusThunk = (id) => (dispatch) => {
+  return axios
+    .delete(`/api/campuses/${id}`)
+    .then((res) => res.data)
+    .then(() => dispatch(deleteCampus(id)))
+    .catch((err) => console.log(err));
+
+};
+
 // REDUCER;
 const reducer = (state = [], action) => {
   switch (action.type) {
@@ -46,9 +76,15 @@ const reducer = (state = [], action) => {
       return action.payload;
     case ADD_CAMPUS:
       return [...state, action.payload];
+    case EDIT_CAMPUS:
+      return state.map((campus) => campus.id === action.payload.id ? action.payload : campus);
+    case DELETE_CAMPUS:
+      return state.filter((campus) => campus.id !== action.payload);
     default:
       return state;
   }
 };
+
+
 
 export default reducer;
